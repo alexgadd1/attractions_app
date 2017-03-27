987070315de6434a27b6d36fa58ed291c14d4472
alexgadd <alex.gadd2@gmail.com>
2017-03-27 17:51:37 -0400

    Updated boilerplate

--- routes/attractions.js
+++ routes/attractions.js
@@ -5,7 +5,31 @@ var Attractions = require('../models/attractions');
 
 // index authors
 router.get('/', function(req, res) {
-    res.send('attractions will be here');
+    // res.send('authors will be here');
+    Attraction.find({})
+        .exec(function(err, authors) {
+            if(err) console.log(err);
+
+            console.log(attractions);
+            // res.send(attractions);
+            res.render('attractions/index', {
+            	attractions: attractions
+            });
+        });
+});
+
+// show author
+router.get('/:id', function(req, res) {
+    Author.findById(req.params.id)
+        .exec(function(err, author) {
+            if(err) console.log(err);
+
+            console.log(author);
+            //res.send(author);
+            res.render('attractions/show', {
+            	attractions: attractions
+            });
+        });
 });
 
 module.exports = router;
