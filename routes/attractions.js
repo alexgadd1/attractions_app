54f9332c2e3eb83242a170508b092901b4da1889
alexgadd <alex.gadd2@gmail.com>
2017-03-28 11:38:26 -0400

    Modified attracrtions.js

--- routes/attractions.js
+++ routes/attractions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var Attraction = require('../models/attraction');
 
-// index authors
+// index attractions
 router.get('/', function(req, res) {
     // res.send('attractions will be here');
     Attraction.find({})
@@ -13,7 +13,7 @@ router.get('/', function(req, res) {
             console.log(attractions);
             // res.send(attractions);
             res.render('attractions/index', {
-            	attractions: attractions
+                  attractions: attractions
             });
         });
 });
@@ -21,32 +21,6 @@ router.get('/', function(req, res) {
 // new attraction
 router.get('/new', function(req, res) {
     res.render('attractions/new');
-});
-
-// edit attraction
-router.get('/:id/edit', function(req,res) {
-    Attraction.findById(req.params.id)
-    .exec(function(err, author) {
-        if (err) { console.log(err); }
-
-        res.render('authors/edit', {
-            attraction: attraction
-        });
-    });
-});
-
-// show attraction
-router.get('/:id', function(req, res) {
-    Attraction.findById(req.params.id)
-        .exec(function(err, author) {
-            if(err) console.log(err);
-
-            console.log(attraction);
-            //res.send(attraction);
-            res.render('attractions/show', {
-            	attractions: attractions
-            });
-        });
 });
 
 // create attraction
@@ -58,14 +32,40 @@ router.post('/', function(req, res) {
         book_title: req.body.book_title,
         publication_year: req.body.publication_year
     });
-    attraction.save(function(err, attraction){
+    attraction.save(function(err, author){
         if (err) { console.log(err); }
 
         console.log(attraction);
-        //res.send(attraction);
-        res.render('attraction/show', {
-        		attraction: attraction
-        })
+        // res.send(attraction);
+        res.render('attractions/show', {
+            attraction: attraction
+        });
+    });
+});
+
+// show attraction
+router.get('/:id', function(req, res) {
+    Attraction.findById(req.params.id)
+        .exec(function(err, attraction) {
+            if(err) console.log(err);
+
+            console.log(attraction);
+            // res.send(attraction);
+            res.render('attractions/show', {
+                attraction: attraction
+            });
+        });
+});
+
+// edit attraction
+router.get('/:id/edit', function(req,res) {
+    Attraction.findById(req.params.id)
+    .exec(function(err, attraction) {
+        if (err) { console.log(err); }
+
+        res.render('attractions/edit', {
+            attraction: attraction
+        });
     });
 });
 
@@ -78,26 +78,27 @@ router.patch('/:id', function(req, res) {
         book_title: req.body.book_title,
         publication_year: req.body.publication_year
     }, {new: true})
-        .exec(function(err, author) {
+        .exec(function(err, attraction) {
             if (err) { console.log(err); }
 
             console.log(attraction);
-            //res.send(attraction);
-        	res.render('attractions/show', {
-        	attraction: attraction
+            // res.send(attraction);
+            res.render('attractions/show', {
+                attraction: attraction
+            });
         });
 });
 
 // delete attraction
 router.delete('/:id', function(req, res) {
     Attraction.findByIdAndRemove(req.params.id)
-        .exec(function(err, author) {
+        .exec(function(err, attraction) {
             if (err) { console.log(err); }
 
             console.log('Attraction deleted.');
-            //res.send('Attraction deleted.');  
-            //redirect back to the index route
-            res.redirect('/attractions');
+            // res.send('Attraction deleted.');
+            // redirect back to the index route
+            res.redirect('/attractions'); 
         });
 });
 
